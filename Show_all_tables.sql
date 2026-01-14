1910e2670395aa59686dcb9a58d000d85dddc796
Konstantin Lobaachev <91482321+klob14012005@users.noreply.github.com>
2026-01-14 16:10:09 +0300

    Delete Show_all_tables.sql

--- Show_all_tables.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-    table_name,
-    column_name,
-    udt_name AS data_type
-FROM information_schema.columns
-WHERE table_schema = 'public'
-  AND data_type = 'USER-DEFINED'
-ORDER BY table_name;
